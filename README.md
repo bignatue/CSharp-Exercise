31ac7b52b44db083ea9431366392e843520d0b77
Brian <bignatue@gmail.com>
2019-08-22 00:37:56 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# C# Exercise
+
+## This repo is for my C# assignment and exercises.
+
+#### - Assignment 1. Find how many primary numbers below 1 billion in C# using Visual Studio.
---
+++ prime-numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int a, b;
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter maximum numbers");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("=========");
+
+            for (a = 2; a < x; a++)
+            {
+                Boolean c = true;
+                for (b = 2; b < a; b++)
+                {
+                    if (a % b == 0)
+                    {
+                        c = false;
+                        break;
+                    }
+                }
+                if (c)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Result");
+            Console.WriteLine(count - 1);
+            Console.WriteLine("=========");
+        }
+    }
+}
