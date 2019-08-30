994ef7c353b4a9281a83648b0f18b87890b571dd
Brian <bignatue@gmail.com>
2019-08-30 07:50:37 -0400

    Enumeration Added

---
+++ enumeration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumeration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ALL DIRECTORY & FILES");
+            Console.WriteLine("=============================================");
+            AllFiles();
+
+            Console.Read();
+        }
+        public static void AllFiles()
+        {
+            string z = @"C:\Users\bigna\OneDrive\Documents";
+            var f = Directory.GetFiles(z, "*.*", SearchOption.AllDirectories);
+            foreach (string y in f)
+            {
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
